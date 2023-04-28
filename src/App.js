f0fab32f62908de544ebd385238c2adb8ac0e464
SunghoonKim <kootae88@gmail.com>
2023-04-28 22:49:23 +0900

    easy component, with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 294.67,
+      date: new Date(2023, 4, 8),
+    },
+    {
+      id: "e2",
+      title: "New Tv",
+      amount: 294.67,
+      date: new Date(2023, 4, 8),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 4, 18),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 294.67,
+      date: new Date(2023, 4, 28),
+    },
+  ];
   return (
     <div className="App">
       <h1>Let's get started</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+<h1> Loading ...</h1>;
